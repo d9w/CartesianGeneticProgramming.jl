2fad1350ddb1ade71ed2f32ac4bb1e0e86c7e67e
Dennis Wilson <dennis@d9w.xyz>
2017-10-02 10:51:14 +0200

    test function determinism

--- test/functions.jl
+++ test/functions.jl
@@ -5,7 +5,9 @@ const global D = 5
 
 files = ["cfg/test.yaml", "cfg/classic.yaml", "cfg/mtcgp.yaml"]
 
-function testout(out)
+function test_inps(f, inps)
+    out = f(inps...)
+    @test all(out == f(inps...))
     minout = minimum(out); maxout = maximum(out)
     @printf("%0.2f, %0.2f, %s, ", minout, maxout, typeof(out))
     @test (minout >= -1.0) && (maxout <= 1.0)
@@ -15,17 +17,14 @@ end
 @testset "Functions" begin
     for file in files
         CGP.Config.init(file)
-        @testset "$file bounds" begin
+        @testset "$file" begin
             for f in CGP.Config.functions
                 print(string(file, ": ", f, ", "))
-                out = f(rand(), rand(), rand())
-                testout(out)
-                out = f(rand(), rand(rand(1:D, rand(1:D))...), rand())
-                testout(out)
-                out = f(rand(rand(1:D, rand(1:D))...), rand(), rand())
-                testout(out)
-                out = f(rand(rand(1:D, rand(1:D))...), rand(rand(1:D, rand(1:D))...), rand())
-                testout(out)
+                test_inps(f, [rand(), rand(), rand()])
+                test_inps(f, [rand(), rand(rand(1:D, rand(1:D))...), rand()])
+                test_inps(f, [rand(rand(1:D, rand(1:D))...), rand(), rand()])
+                test_inps(f, [rand(rand(1:D, rand(1:D))...), rand(rand(1:D, rand(1:D))...),
+                           rand()])
                 println()
             end
         end
