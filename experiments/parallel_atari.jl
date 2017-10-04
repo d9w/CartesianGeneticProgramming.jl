226c89e0c4e7d9239a978d909d69e4518301e313
Dennis Wilson <dennis@d9w.xyz>
2017-10-04 18:52:20 +0200

    game name as command line option

--- experiments/parallel_atari.jl
+++ experiments/parallel_atari.jl
@@ -7,15 +7,7 @@ function play_atari(c::Chromosome, game::Game, display::Bool, frame_dir::String)
     reset_game(game.ale)
     reward = 0
     frames = 0
-    # breakout needs to "fire" to start
-    # act(game.ale, game.actions[2])
-    # life = lives(game.ale)*1.0
     while ~game_over(game.ale)
-        # in breakout, resend fire
-        # if lives(game.ale) < life
-        #     life = lives(game.ale)
-        #     act(game.ale, game.actions[2])
-        # end
         output = process(c, get_inputs(game))
         action = game.actions[indmax(output)]
         for i in 1:4
@@ -40,18 +32,21 @@ function play_and_draw(c::Chromosome, game::Game, display::Bool, frame_dir::Stri
     play_atari(c, game, display, frame_dir)
 end
 
-seed = 0
-log = "log"
-ea = oneplus
-ctype = CGPChromo
-game_name = "qbert"
-frame_dir = "frames"
-if length(ARGS) > 0; seed = parse(Int64, ARGS[1]); end
-if length(ARGS) > 1; log = ARGS[2]; end
-if length(ARGS) > 2; ea = eval(parse(ARGS[3])); end
-if length(ARGS) > 3; ctype = eval(parse(ARGS[4])); end
-if length(ARGS) > 4; game_name = ARGS[5]; end
-if length(ARGS) > 5; frame_dir = ARGS[6]; end
+function run_ea(seed, jlk)
+Logging.configure(filename=log, level=INFO)
+Logging.info("I: $seed $ea $ctype")
+srand(seed)
+
+game = Game("qbert")
+nin = length(get_inputs(game))
+nout = length(game.actions)
+fit = x->play_atari(x, game, false, frame_dir)
+record = x->play_and_draw(x, game, true, frame_dir)
+maxfit, best = ea(ctype, nin, nout, fit, true, record)
+close!(game)
+best
+
+
 
 CGP.Config.init("cfg/base.yaml")
 if ctype == MTPCGPChromo
@@ -61,13 +56,25 @@ else
 end
 
 Logging.configure(filename=log, level=INFO)
-Logging.info("I: $seed $ea $ctype $game_name")
+Logging.info("I: $seed $ea $ctype")
 srand(seed)
 
-game = Game(game_name)
+game = Game("qbert")
 nin = length(get_inputs(game))
 nout = length(game.actions)
 fit = x->play_atari(x, game, false, frame_dir)
 record = x->play_and_draw(x, game, true, frame_dir)
 maxfit, best = ea(ctype, nin, nout, fit, true, record)
 close!(game)
+best
+
+seed = 0
+log = "log"
+ea = oneplus
+ctype = CGPChromo
+frame_dir = "frames"
+if length(ARGS) > 0; seed = parse(Int64, ARGS[1]); end
+if length(ARGS) > 1; log = ARGS[2]; end
+if length(ARGS) > 2; ea = eval(parse(ARGS[3])); end
+if length(ARGS) > 3; ctype = eval(parse(ARGS[4])); end
+if length(ARGS) > 4; frame_dir = ARGS[5]; end
