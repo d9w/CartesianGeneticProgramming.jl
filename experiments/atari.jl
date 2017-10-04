a88a4dfc9c8fd871a3bdf0b91fb3bb3073db5df7
Dennis Wilson <dennis@d9w.xyz>
2017-10-04 17:50:19 +0200

    frame directory argument for atari

--- experiments/atari.jl
+++ experiments/atari.jl
@@ -3,7 +3,7 @@ using CGP
 using Logging
 using Images
 
-function play_atari(c::Chromosome, game::Game, display::Bool)
+function play_atari(c::Chromosome, game::Game, display::Bool, frame_dir::String)
     reset_game(game.ale)
     reward = 0
     frames = 0
@@ -22,7 +22,7 @@ function play_atari(c::Chromosome, game::Game, display::Bool)
             reward += act(game.ale, game.actions[indmax(output)])
             frames += 1
             if display
-                save(@sprintf("frames/frame_%05d.png", frames), draw(game))
+                save(@sprintf("%s/frame_%05d.png", frame_dir, frames), draw(game))
             end
         end
         if frames > 108000
@@ -37,10 +37,12 @@ seed = 0
 log = "log"
 ea = oneplus
 ctype = CGPChromo
+frame_dir = "frames"
 if length(ARGS) > 0; seed = parse(Int64, ARGS[1]); end
 if length(ARGS) > 1; log = ARGS[2]; end
 if length(ARGS) > 2; ea = eval(parse(ARGS[3])); end
 if length(ARGS) > 3; ctype = eval(parse(ARGS[4])); end
+if length(ARGS) > 4; frame_dir = ARGS[5]; end
 
 CGP.Config.init("cfg/base.yaml")
 if ctype == MTPCGPChromo
@@ -52,11 +54,12 @@ end
 Logging.configure(filename=log, level=INFO)
 Logging.info("I: $seed $ea $ctype")
 srand(seed)
+mkpath(frame_dir)
 
 game = Game("qbert")
 nin = length(get_inputs(game))
 nout = length(game.actions)
-fit = x->play_atari(x, game, false)
-record = x->play_atari(x, game, true)
+fit = x->play_atari(x, game, false, frame_dir)
+record = x->play_atari(x, game, true, frame_dir)
 maxfit, best = ea(ctype, nin, nout, fit, true, record)
 close!(game)
