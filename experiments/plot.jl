186e9a4f8917140c2191ee184278100c2b99520d
Dennis Wilson <dennis@d9w.xyz>
2017-10-30 16:10:13 +0100

    plot cmaes parameter results

--- experiments/plot.jl
+++ experiments/plot.jl
@@ -9,7 +9,8 @@ Gadfly.push_theme(Theme(major_label_font="Droid Sans",
                         major_label_font_size=18pt, minor_label_font_size=16pt,
                         line_width=0.8mm, key_label_font="Droid Sans",
                         lowlight_color=c->RGBA{Float32}(c.r, c.g, c.b, 0.2),
-                        key_label_font_size=14pt, key_position=:right))
+                        key_label_font_size=14pt, key_position=:none,
+                        default_color=colorant"#000000"))
 
 colors = [colorant"#e41a1c", colorant"#377eb8", colorant"#4daf4a",
           colorant"#984ea3", colorant"#ff7f00", colorant"#ffff33"]
@@ -130,4 +131,57 @@ function plot_tests(tests::Array{Float64}, labels::Array{String}, title::String)
     plt
 end
 
+function get_cmaes_results(log::String)
+    res = readtable(log, header=false, separator=' ',
+                    names=[:date, :time, :seed, :eval, :fit, :active, :nodes, :ea,
+                           :chromo, :mutation, :crossover, :distance, :recurrency,
+                           :input_mutation_rate, :output_mutation_rate, :node_mutation_rate,
+                           :add_node_rate, :delete_node_rate, :add_mutation_rate,
+                           :delete_mutation_rate, :speciation_thresh, :ga_elitism_rate,
+                           :ga_crossover_rate, :ga_mutation_rate])
+    res[:chromosome] = map(x->split(split(x, '.')[2], "Chromo")[1], res[:chromo])
+    runs = by(res, [:ea, :chromosome, :mutation, :crossover, :distance, :recurrency,
+                      :input_mutation_rate], df->maximum(df[:fit]))
+    top_runs = by(runs, [:ea, :chromosome], df->sort!(df[:x1], rev=true)[1:20])
+
+    res, top_runs
+end
+
+function plot_top_runs(top_runs::DataFrame, ti::String, filename::String;
+                       ymin = floor(minimum(top_runs[:x1])*100)/100,
+                       ymax = ceil(maximum(top_runs[:x1])*100)/100,
+                       step = round((ymax-ymin)/5*10)/10)
+
+    cgp = @from i in top_runs begin
+        @where i.chromosome == "CGP"
+        @select {i.x1, i.ea, i.chromosome}
+        @collect DataFrame
+    end
+    pcgp = @from i in top_runs begin
+        @where i.chromosome == "PCGP"
+        @select {i.x1, i.ea, i.chromosome}
+        @collect DataFrame
+    end
+
+    if step == 0
+        step = (ymax-ymin)/5
+    end
+
+    yticks = [ymin:step:ymax;]
+
+    plt1 = plot(layer(cgp, x=:ea, y=:x1, color=:ea, Geom.boxplot),
+                Scale.color_discrete_manual(colors...),
+                Coord.cartesian(ymin=ymin, ymax=ymax),
+                Guide.yticks(ticks=yticks, label=true),
+                Guide.ylabel("Fitness"), Guide.xlabel("CGP"));
+    plt2 = plot(layer(pcgp, x=:ea, y=:x1, color=:ea, Geom.boxplot),
+                Scale.color_discrete_manual(colors...),
+                Coord.cartesian(ymin=ymin, ymax=ymax),
+                Guide.yticks(ticks=yticks, label=false),
+                Guide.ylabel(nothing), Guide.xlabel("PCGP"));
+
+    plt = title(hstack(plt1, plt2), ti);
+    draw(PDF(filename, 8inch, 6inch), plt)
+    plt
+end
 
