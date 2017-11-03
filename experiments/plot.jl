4417a40d541a9d3d699faec71a5a99ecc249a2fa
Dennis Wilson <dennis@d9w.xyz>
2017-11-03 14:27:44 +0100

    plot parameter distibutions from cmaes evolutions

--- experiments/plot.jl
+++ experiments/plot.jl
@@ -14,8 +14,28 @@ Gadfly.push_theme(Theme(major_label_font="Droid Sans",
 
 colors = [colorant"#e41a1c", colorant"#377eb8", colorant"#4daf4a",
           colorant"#984ea3", colorant"#ff7f00", colorant"#ffff33"]
-# colors = [colorant"#7fc97f", colorant"#beaed4", colorant"#fdc086",
-#           colorant"#ffff99", colorant"#386cb0", colorant"#f0027f"]
+
+all_params = [:recurrency, :input_mutation_rate, :output_mutation_rate,
+              :node_mutation_rate, :add_node_rate, :delete_node_rate,
+              :add_mutation_rate, :delete_mutation_rate,
+              :speciation_thresh, :ga_elitism_rate, :ga_crossover_rate,
+              :ga_mutation_rate, :f_mutation, :f_crossover, :f_distance]
+
+ea_params = [:recurrency, :input_mutation_rate, :output_mutation_rate,
+             :node_mutation_rate, :add_node_rate, :delete_node_rate,
+             :add_mutation_rate, :delete_mutation_rate,
+             :f_mutation]
+
+ga_params = [:recurrency, :input_mutation_rate, :output_mutation_rate,
+             :node_mutation_rate, :add_node_rate, :delete_node_rate,
+             :add_mutation_rate, :delete_mutation_rate, :ga_elitism_rate,
+             :ga_crossover_rate, :ga_mutation_rate, :f_mutation, :f_crossover]
+
+neat_params = [:recurrency, :input_mutation_rate, :output_mutation_rate,
+               :node_mutation_rate, :add_node_rate, :delete_node_rate,
+               :add_mutation_rate, :delete_mutation_rate,
+               :speciation_thresh, :ga_crossover_rate,
+               :ga_mutation_rate, :f_mutation, :f_crossover, :f_distance]
 
 function reducef(df, xmax)
     r = 1:length(df[:eval])
@@ -140,48 +160,98 @@ function get_cmaes_results(log::String)
                            :delete_mutation_rate, :speciation_thresh, :ga_elitism_rate,
                            :ga_crossover_rate, :ga_mutation_rate])
     res[:chromosome] = map(x->split(split(x, '.')[2], "Chromo")[1], res[:chromo])
-    runs = by(res, [:ea, :chromosome, :mutation, :crossover, :distance, :recurrency,
-                      :input_mutation_rate], df->maximum(df[:fit]))
-    top_runs = by(runs, [:ea, :chromosome], df->sort!(df[:x1], rev=true)[1:20])
+    maxeval = maximum(res[:eval])
+    runs = @from i in res begin
+        @where i.eval == maxeval
+        @select i
+        @collect DataFrame
+    end
+    mutations = sort!(unique(runs[:mutation]))
+    println(mutations)
+    runs[:f_mutation] = indexin(runs[:mutation], mutations)./(1.0*length(mutations))
+    crossovers = sort!(unique(runs[:crossover]))
+    println(crossovers)
+    runs[:f_crossover] = indexin(runs[:crossover], crossovers)./(1.0*length(crossovers))
+    distances = sort!(unique(runs[:distance]))
+    println(distances)
+    runs[:f_distance] = indexin(runs[:distance], distances)./(1.0*length(distances))
+    eas = unique(runs[:ea])
+    chromosomes = unique(runs[:chromosome])
 
-    res, top_runs
+    top_runs = DataFrame()
+    for ea in eas
+        for chromo in chromosomes
+            group = @from i in runs begin
+                @where i.ea == ea && i.chromosome == chromo
+                @select i
+                @collect DataFrame
+            end
+            sort!(group, cols=[:fit], rev=true)
+            group[:id] = 1:size(group, 1)
+            top_group = @from i in group begin
+                @where i.id <= 20
+                @select i
+                @collect DataFrame
+
+            end
+            top_runs = vcat(top_runs, top_group)
+        end
+    end
+
+    runs, top_runs
 end
 
 function plot_top_runs(top_runs::DataFrame, ti::String, filename::String;
-                       ymin = floor(minimum(top_runs[:x1])*100)/100,
-                       ymax = ceil(maximum(top_runs[:x1])*100)/100,
+                       ymin = floor(minimum(top_runs[:fit])*100)/100,
+                       ymax = ceil(maximum(top_runs[:fit])*100)/100,
                        step = round((ymax-ymin)/5*10)/10)
-
-    cgp = @from i in top_runs begin
-        @where i.chromosome == "CGP"
-        @select {i.x1, i.ea, i.chromosome}
-        @collect DataFrame
-    end
-    pcgp = @from i in top_runs begin
-        @where i.chromosome == "PCGP"
-        @select {i.x1, i.ea, i.chromosome}
-        @collect DataFrame
-    end
-
-    if step == 0
-        step = (ymax-ymin)/5
-    end
 
     yticks = [ymin:step:ymax;]
 
-    plt1 = plot(layer(cgp, x=:ea, y=:x1, color=:ea, Geom.boxplot),
-                Scale.color_discrete_manual(colors...),
-                Coord.cartesian(ymin=ymin, ymax=ymax),
-                Guide.yticks(ticks=yticks, label=true),
-                Guide.ylabel("Fitness"), Guide.xlabel("CGP"));
-    plt2 = plot(layer(pcgp, x=:ea, y=:x1, color=:ea, Geom.boxplot),
-                Scale.color_discrete_manual(colors...),
-                Coord.cartesian(ymin=ymin, ymax=ymax),
-                Guide.yticks(ticks=yticks, label=false),
-                Guide.ylabel(nothing), Guide.xlabel("PCGP"));
-
-    plt = title(hstack(plt1, plt2), ti);
-    draw(PDF(filename, 8inch, 6inch), plt)
+    plt = plot(top_runs, xgroup=:chromosome,
+               x=:ea, y=:fit, color=:ea,
+               Geom.subplot_grid(Coord.cartesian(ymin=ymin, ymax=ymax),
+                                 Geom.boxplot),
+               Scale.color_discrete_manual(colors...),
+               Guide.ylabel(nothing), Guide.xlabel(nothing),
+               Guide.title(ti))
+    draw(PDF(string(filename, "_fitness.pdf"), 8inch, 6inch), plt)
     plt
 end
 
+function plot_top_params(top_runs::DataFrame, ti::String, filename::String)
+    melted = melt(top_runs, [:ea, :chromosome])
+    top_params = @from i in melted begin
+        @where ((i.ea == "oneplus" && i.variable in ea_params) ||
+                (i.ea == "GA" && i.variable in ga_params) ||
+                (i.ea == "NEAT" && i.variable in neat_params))
+        @select i
+        @collect DataFrame
+    end
+    top_params[:parameter] = indexin(top_params[:variable], all_params)
+
+    plt = plot(top_params, xgroup=:ea, ygroup=:chromosome,
+               x=:parameter, y=:value, color=:variable,
+               Geom.subplot_grid(Geom.boxplot,
+               Coord.cartesian(ymin=0.0, ymax=1.0, xmin=0, xmax=length(all_params))),
+               Guide.ylabel(nothing), Guide.xlabel(nothing),
+               style(key_position=:right),
+               Guide.title(ti))
+    draw(PDF(string(filename, "_params.pdf"), 12inch, 8inch), plt)
+    top_params
+end
+
+function get_bests(logs::Array{String})
+    bests = DataFrame()
+    for l in logs
+        res, top_runs = get_cmaes_results(string("results/cmaes/", l, ".log"))
+        tops = @from i in top_runs begin
+            @where i.id <= 5
+            @select i
+            @collect DataFrame
+        end
+        tops[:problem] = l
+        bests = vcat(bests, tops)
+    end
+    bests
+end
