914edeea4ed9b978b8a0d809b61ae546875ab038
Dennis Wilson <dennis@d9w.xyz>
2018-06-14 17:45:47 +0200

    remove development files

--- scripts/modload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# source me
-module purge
-module load intel/14.0.2.144
-module load julia
-module load R
-module load openmpi/3.0.0
-export PATH=/usr/local/julia/0.5.0_stand/julia-3c9d75391c/bin:$PATH
