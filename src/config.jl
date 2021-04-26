24dc0ef331ab9e03b264056e00d4102cdfcbc031
erwanlecarpentier <erwan.lc2@gmail.com>
2021-04-26 15:47:39 +0200

    Adding CGPFunction module as an option in config reader

--- src/config.jl
+++ src/config.jl
@@ -6,12 +6,18 @@ function get_config(config::Dict)
     # parse all function names, assign to function value
     two_arity = falses(length(config["functions"]))
     functions = Array{Function}(undef, length(config["functions"]))
+    # use function_module if given (default is CGPFunctions)
+    if "function_module" in keys(config)
+        function_module = config["function_module"]
+    else
+        function_module = CGPFunctions
+    end
     for i in eachindex(config["functions"])
         fname = config["functions"][i]
-        if CGPFunctions.arity[fname] == 2
+        if function_module.arity[fname] == 2
             two_arity[i] = true
         end
-        functions[i] = eval(Meta.parse(string("CGPFunctions.", fname)))
+        functions[i] = getfield(function_module, Symbol(fname))
     end
     config["two_arity"] = two_arity
     config["functions"] = functions
