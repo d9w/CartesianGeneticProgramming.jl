83871d6444f87fa1b4914c4f5a5a8a630c6d1ec5
erwanlecarpentier <erwan.lc2@gmail.com>
2021-05-17 13:48:00 +0200

    Fixed individual test function (missing parameter: cfg)

--- test/individual.jl
+++ test/individual.jl
@@ -2,7 +2,7 @@ using Test
 using CartesianGeneticProgramming
 import YAML
 
-function test_ind(ind::CGPInd)
+function test_ind(ind::CGPInd, cfg::NamedTuple)
     @test length(ind.nodes) == 3 * cfg.columns + cfg.n_in
     for node in ind.nodes
         if node.active
@@ -22,7 +22,7 @@ end
     test_filename = string(@__DIR__, "/test.yaml")
     cfg = get_config(test_filename)
     ind = CGPInd(cfg)
-    test_ind(ind)
+    test_ind(ind, cfg)
 end
 
 """
@@ -53,7 +53,7 @@ Similar to CGPInd construction but uses custom set of CGP functions
     test_filename = string(@__DIR__, "/test.yaml")
     cfg = get_config(test_filename, function_module=MinimalFunctionModuleExample)
     ind = CGPInd(cfg)
-    test_ind(ind)
+    test_ind(ind, cfg)
 end
 
 """
